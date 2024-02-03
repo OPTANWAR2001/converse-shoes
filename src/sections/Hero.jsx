4f11b97afd6fe6f70988081a3a471d573470b673
OPTANWAR2001 <optanwar10@gmail.com>
2024-02-03 21:30:06 +0530

    add text

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
           </span>{" "}
         </h1>
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
-          Discover stylish Converse arrivals, quality comfort, and innovation
+          Discover stylish Converse new arrivals, quality comfort, and innovation
           for your active life.
         </p>
         <Button label="Shop now" iconURL={arrowRight} />
